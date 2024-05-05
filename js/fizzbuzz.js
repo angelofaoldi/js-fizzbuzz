f6b9c7bb9ca091245ef93dbad5a36066e5237b52
angelofaoldi <faoldiangelo@gmail.com>
2024-05-05 15:25:30 +0200

    JS: IF-ELSE structure

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -9,4 +9,37 @@
 */
 
 for (let i = 1; 1 <= 100; i++)
-{}
+
+    // questo ciclo dovrò avere delle condizioni
+    // impostiamo la struttura if {} else {}
+
+    { 
+        if ((i % 3) === 0 && (i % 5) === 0)
+        {console.log('FizzBuzz');}
+    
+        /* con && le due condizioni di i vengono connesse
+        quindi FizzBuzz verrà stampato in console
+        solo se le condizioni saranno uguali a zero */
+
+        else if ((i % 5) === 0)
+        {console.log('Buzz');}
+
+        /* qui andiamo a dare ad i un valore
+        che equivale a tutti i multipli (%) di 5
+        e questo valore verrà poi sovrascritto da Buzz */
+
+        else if ((i % 3) === 0)
+        {console.log(?Fizz);}
+
+        /* qui andiamo a dare ad i un valore
+        che equivale a tutti i multipli (%) di 3
+        e questo valore verrà poi sovrascritto da Buzz */
+
+        else
+        {console.log(i);}
+
+        /* se tutte le condizioni sopra non vengono
+        rispettate, allora stampa semplicemente un valore
+        numerico incrementale */
+        
+    }
