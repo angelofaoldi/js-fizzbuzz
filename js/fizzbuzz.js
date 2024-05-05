a8c0db291b48aed78ecf2e2f28a3019ed0105e86
angelofaoldi <faoldiangelo@gmail.com>
2024-05-05 15:10:42 +0200

    JS: for cycle

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// CICLO FOR
+
+/*
+- variabile i = 1 come punto di partenza
+- limite alla variabile, quindi minore uguale 100
+- crescita della variabile, quindi i++
+*/
+
+for (let i = 1; 1 <= 100; i++)
+{}
