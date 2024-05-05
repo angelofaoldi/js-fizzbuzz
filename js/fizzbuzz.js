37631a39f626269b38f36d13cddc2e0cbdbf8138
angelofaoldi <faoldiangelo@gmail.com>
2024-05-05 16:44:05 +0200

    JS: append property

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/* creo una costante collegata con il primo elemento
+dalla classe griglia */
+
+const grid = document.querySelector('div.griglia');
+
 // CICLO FOR
 
 /*
@@ -10,33 +15,73 @@
 
 for (let i = 1; 1 <= 100; i++)
 
-    // questo ciclo dovrà avere delle condizioni
-    // impostiamo la struttura if {} else {}
+    {   
+        /* imposto una costante per creare un div
+        che fungerà da cella per incasellare i numeri, una box */
 
-    { 
+        const div = document.createElement('div');
+
+        // questo ciclo dovrà avere delle condizioni
+        // impostiamo la struttura if {} else {}
+
         if ((i % 3) === 0 && (i % 5) === 0)
-        {console.log('FizzBuzz');}
+        {
+            console.log('FizzBuzz');
+
+            /*durante la stampa, la proprietà append
+            aggiungerà alla fine sia una cella div
+            sia una stringa di testo FizzBuzz*/
+
+            div.append('FizzBuzz');
+            grid.append(div);
+        }
     
         /* con && le due condizioni di i vengono connesse
         quindi FizzBuzz verrà stampato in console
         solo se le condizioni saranno uguali a zero */
 
         else if ((i % 5) === 0)
-        {console.log('Buzz');}
+        {
+            console.log('Buzz');
+
+            /*durante la stampa, la proprietà append
+            aggiungerà alla fine sia una cella div
+            sia una stringa di testo Buzz*/
+
+            div.append('Buzz');
+            grid.append(div);
+        }
 
         /* qui andiamo a dare ad i un valore
         che equivale a tutti i multipli (%) di 5
         e questo valore verrà poi sovrascritto da Buzz */
 
         else if ((i % 3) === 0)
-        {console.log('Fizz');}
+        {
+            console.log('Fizz');
+        
+            /*durante la stampa, la proprietà append
+            aggiungerà alla fine sia una cella div
+            sia una stringa di testo Fizz*/
+
+            div.append('Fizz');
+            grid.append(div);
+        }
 
         /* qui andiamo a dare ad i un valore
         che equivale a tutti i multipli (%) di 3
         e questo valore verrà poi sovrascritto da Fizz */
 
         else
-        {console.log(i);}
+        {
+            console.log(i);
+
+            /*durante la stampa, la proprietà append
+            aggiungerà alla fine sia una cella div */
+
+            div.append(i);
+            grid.append(div);
+        }
 
         /* se tutte le condizioni sopra non vengono
         rispettate, allora stampa semplicemente il valore
